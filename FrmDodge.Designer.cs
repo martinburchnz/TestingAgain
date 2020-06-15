1b32647762fe476dd09b575220c48eaaa9314665
Martin Burch <MartinB@mmc.school.nz>
2020-06-15 19:42:27 +1200

    Add project files.

---
+++ FrmDodge.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace TestingAgain
+{
+    partial class FrmDodge
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PnlGame = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // PnlGame
+            // 
+            this.PnlGame.BackColor = System.Drawing.Color.Silver;
+            this.PnlGame.Location = new System.Drawing.Point(1, 65);
+            this.PnlGame.Name = "PnlGame";
+            this.PnlGame.Size = new System.Drawing.Size(500, 400);
+            this.PnlGame.TabIndex = 0;
+            // 
+            // FrmDodge
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 461);
+            this.Controls.Add(this.PnlGame);
+            this.Name = "FrmDodge";
+            this.Text = "Dodge";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PnlGame;
+    }
+}
+
---
+++ Planet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace TestingAgain
+{
+    class Planet
+    {
+        // declare fields to use in the class
+        public int x, y, width, height;//variables for the rectangle
+        public Image planetImage;//variable for the planet's image
+
+        public Rectangle planetRec;//variable for a rectangle to place our image in
+        public int score;
+        //Create a constructor (initialises the values of the fields)
+        public Planet()
+        {
+            x = 10;
+            y = 10;
+            width = 20;
+            height = 20;
+            planetImage = Image.FromFile("planet1.png");
+            planetRec = new Rectangle(x, y, width, height);
+        }
+
+    }
+}
